c9d9fc646f038b8df0689312c01c43a9b30d83c5
Jai Bhagat <jaybhagat841@gmail.com>
2022-01-03 09:42:28 -0500

    fix:  remove sourceMaps false in ember-cli-build
    There was previously a bug with sourceMaps that slowed down the build.
    This issue has been resolved by Ember and now we can expect the default
    behavior of sourceMaps being enabled in dev and disabled in prod.

--- ui/ember-cli-build.js
+++ ui/ember-cli-build.js
@@ -5,7 +5,7 @@ const environment = EmberApp.env();
 const isProd = environment === 'production';
 const isTest = environment === 'test';
 
-module.exports = function(defaults) {
+module.exports = function (defaults) {
   var app = new EmberApp(defaults, {
     svg: {
       paths: ['node_modules/@hashicorp/structure-icons/dist', 'public/images/icons'],
@@ -28,9 +28,6 @@ module.exports = function(defaults) {
     },
     hinting: isTest,
     tests: isTest,
-    sourcemaps: {
-      enabled: false,
-    },
   });
 
   // Use `app.import` to add additional libraries to the generated
